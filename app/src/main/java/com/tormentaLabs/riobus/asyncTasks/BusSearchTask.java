51d469f5ee6c6450b8858464594d1695f05e571b
Fred Souza <fredericoamsouza@gmail.com>
2015-07-29 23:43:09 -0300

    Fixing API data retrieval issue due to date API change through versions

--- app/src/main/java/com/tormentaLabs/riobus/asyncTasks/BusSearchTask.java
+++ app/src/main/java/com/tormentaLabs/riobus/asyncTasks/BusSearchTask.java
@@ -13,6 +13,7 @@ import com.tormentaLabs.riobus.common.BusDataReceptor;
 import com.tormentaLabs.riobus.model.Bus;
 import com.tormentaLabs.riobus.service.HttpService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.RestAdapter;
@@ -22,10 +23,49 @@ import retrofit.converter.GsonConverter;
 public class BusSearchTask extends AsyncTask<String, Void, List<Bus>>{
 
     private Context context;
+    private HttpService service;
     ProgressDialog dialog;
 
     public BusSearchTask(Context context){
         this.context = context;
+    }
+
+    private List<Bus> getOldAPI(String lines){
+        Gson gson = new GsonBuilder()
+                .setDateFormat("MM-dd-yyyy HH:mm:ss")
+                .create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(EnvironmentConfig.URL_ENDPOINT)
+                .setConverter(new GsonConverter(gson))
+                .build();
+        service = restAdapter.create(HttpService.class);
+
+        try{
+            return service.getOldPage(lines);
+        } catch (RetrofitError e){
+            e.printStackTrace();
+            return new ArrayList<Bus>();
+        }
+    }
+
+    private List<Bus> getNewAPI(String lines){
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                .create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(EnvironmentConfig.URL_ENDPOINT)
+                .setConverter(new GsonConverter(gson))
+                .build();
+        service = restAdapter.create(HttpService.class);
+
+        try{
+            return service.getPage(lines);
+        } catch (RetrofitError e){
+            e.printStackTrace();
+            return new ArrayList<Bus>();
+        }
     }
 
     @Override
@@ -40,25 +80,10 @@ public class BusSearchTask extends AsyncTask<String, Void, List<Bus>>{
 
     @Override
     protected List<Bus> doInBackground(String... params) {
-        String data = params[0].replaceAll("\\s","");
+        String data = params[0].replaceAll("\\s", "");
 
-        Gson gson = new GsonBuilder()
-                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-                .create();
-
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(EnvironmentConfig.URL_ENDPOINT)
-                .setConverter(new GsonConverter(gson))
-                .build();
-
-
-        HttpService service = restAdapter.create(HttpService.class);
-        try {
-            return service.getPage(data);
-        } catch (RetrofitError e) {
-            e.printStackTrace();
-            return null;
-        }
+        List<Bus> buses = getNewAPI(data);
+        return (buses.size()>0)? buses : getOldAPI(data);
     }
 
     @Override
--- app/src/main/java/com/tormentaLabs/riobus/service/HttpService.java
+++ app/src/main/java/com/tormentaLabs/riobus/service/HttpService.java
@@ -16,18 +16,32 @@ import retrofit.http.Query;
 public interface HttpService {
 
     @Headers({
-        "Accept:application/json",
-        "Cache-Control: max-age=640000",
-        "userAgent: riobus-android-2.0"
+            "Accept:application/json",
+            "Cache-Control: max-age=640000",
+            "userAgent: riobus-android-2.0"
     })
     @GET("/v3/search/{lines}")
     void getPage(@Path("lines")String lines, Callback< List<Bus> > buses);
 
     @Headers({
-        "Accept:application/json",
-        "Cache-Control: max-age=640000",
-        "userAgent: riobus-android-2.0"
+            "Accept:application/json",
+            "Cache-Control: max-age=640000",
+            "userAgent: riobus-android-2.0"
     })
     @GET("/v3/search/{lines}")
     List<Bus> getPage(@Path("lines")String lines);
+
+    @Headers({
+            "Accept:application/json",
+            "Cache-Control: max-age=640000"
+    })
+    @GET("/search/2/{lines}")
+    void getOldPage(@Path("lines")String lines, Callback< List<Bus> > buses);
+
+    @Headers({
+            "Accept:application/json",
+            "Cache-Control: max-age=640000"
+    })
+    @GET("/search/2/{lines}")
+    List<Bus> getOldPage(@Path("lines")String lines);
 }
