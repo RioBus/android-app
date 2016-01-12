379a08e44da8f045b835a2bf220fc8bb6fb2e35f
Limazix <blimacardoso@gmail.com>
2016-01-12 18:55:24 -0200

    Moving History table name and colunms to an utils file

--- app/src/main/java/com/tormentaLabs/riobus/history/model/HistoryModel.java
+++ app/src/main/java/com/tormentaLabs/riobus/history/model/HistoryModel.java
@@ -6,19 +6,20 @@ import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.tormentaLabs.riobus.core.model.LineModel;
+import com.tormentaLabs.riobus.history.utils.HistoryUtils;
 
 /**
  * @author limazix
  * @since 2.1
  * Created on 05/10/15
  */
-@Table(name = "HISTORY", id = BaseColumns._ID)
+@Table(name = HistoryUtils.TABLE_HISTORY_NAME, id = BaseColumns._ID)
 public class HistoryModel extends Model {
 
-    @Column(name = "LINE")
+    @Column(name = HistoryUtils.TABLE_HISTORY_COL_LINE)
     public LineModel line;
 
-    @Column(name = "CREATED_AT")
+    @Column(name = HistoryUtils.TABLE_HISTORY_COL_CREATED_AT)
     public String createdAt;
 
     public HistoryModel() {
---
+++ app/src/main/java/com/tormentaLabs/riobus/history/utils/HistoryUtils.java
@@ -0,0 +1,10 @@
+package com.tormentaLabs.riobus.history.utils;
+
+/**
+ * Created by limazix on 12/01/16.
+ */
+public class HistoryUtils {
+    public static final String TABLE_HISTORY_NAME = "HISTORY";
+    public static final String TABLE_HISTORY_COL_LINE = "LINE";
+    public static final String TABLE_HISTORY_COL_CREATED_AT = "CREATED_AT";
+}
