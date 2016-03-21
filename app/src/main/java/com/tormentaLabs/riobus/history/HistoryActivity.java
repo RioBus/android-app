c0106996b3020b5c6be287df364f1919cdccd241
George Rappel <george.concei@hotmail.com>
2016-03-21 11:28:32 -0300

    Add option to delete search history with a dialog

--- app/src/main/java/com/tormentaLabs/riobus/history/HistoryActivity.java
+++ app/src/main/java/com/tormentaLabs/riobus/history/HistoryActivity.java
@@ -1,11 +1,16 @@
 package com.tormentaLabs.riobus.history;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ExpandableListView;
 
 import com.tormentaLabs.riobus.R;
 import com.tormentaLabs.riobus.history.adapter.HistoryListAdapter;
+import com.tormentaLabs.riobus.history.controller.HistoryController;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
@@ -36,5 +41,37 @@ public class HistoryActivity extends AppCompatActivity {
         setSupportActionBar(rioBusToolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         historyListView.setAdapter(historyListAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_history, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.history_clear:
+                clearHistory();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void clearHistory(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.history_clear_dialog);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                new HistoryController().deleteHistory();
+                historyListAdapter.populateList();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, null);
+        builder.create().show();
     }
 }
