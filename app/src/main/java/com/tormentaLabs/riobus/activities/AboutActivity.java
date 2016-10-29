4adefd3d0dfae51eb846506553eda93797ad2d13
Fred Souza <fredericoamsouza@gmail.com>
2016-10-29 16:57:36 -0200

    Filling up AboutActivity

--- app/src/main/java/com/tormentaLabs/riobus/activities/AboutActivity.java
+++ app/src/main/java/com/tormentaLabs/riobus/activities/AboutActivity.java
@@ -2,17 +2,27 @@ package com.tormentaLabs.riobus.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.tormentaLabs.riobus.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtContent) TextView txtContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtContent.setText(Html.fromHtml(getString(R.string.activity_about_content)));
     }
 
     @Override
