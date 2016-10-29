cbd2e0857ebc35ffdbcb58ae30f4d22eb343bad0
Fred Souza <fredericoamsouza@gmail.com>
2016-10-29 16:00:48 -0200

    Showing back button in toolbar

--- app/src/main/java/com/tormentaLabs/riobus/activities/MapActivity.java
+++ app/src/main/java/com/tormentaLabs/riobus/activities/MapActivity.java
@@ -2,6 +2,7 @@ package com.tormentaLabs.riobus.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.tormentaLabs.riobus.R;
 
@@ -15,7 +16,20 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         queryString = getIntent().getStringExtra(LINE_TITLE);
         setTitle(queryString);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
